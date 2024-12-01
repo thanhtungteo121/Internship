bbae1b8ac366abdeb39d42e3ecca239edbd9c772
teoduongthanh <112098570+teoduongthanh@users.noreply.github.com>
2024-12-01 23:42:42 +0700

    Update UserRouter.js
    Add authmiddleware in router private.

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -16,16 +16,17 @@ router.post('/logout-user',userController.logoutUser)
 router.post('/loggin-user',userController.logginUser)
 
 router.put('/update-user/:_id',authUserMiddleware,userController.updateUser)
+
 //khi chạy pua api delete sẽ chạy qua một midleware để xác nhận các điều kiện cần ddeeer xóa user
 router.delete('/delete-user/:_id',authMiddleware,userController.deleteUser)
 
 router.post('/delete-many-user',authMiddleware,userController.deleteManyUser)
 
-router.get('/get-all-user',userController.getAllUser)
+router.get('/get-all-user',authMiddleware,userController.getAllUser)
 
-router.get('/get-detail-user/:_id' ,userController.getDetailUser)
+router.get('/get-detail-user/:_id' ,authUserMiddleware,userController.getDetailUser)
 
-router.post('/refresh-token',userController.refreshToken)
+router.post('/refresh-token',authUserMiddleware,userController.refreshToken)
 
 // Yêu cầu gửi email reset mật khẩu
 router.post('/request-password-reset', userController.requestPasswordReset);

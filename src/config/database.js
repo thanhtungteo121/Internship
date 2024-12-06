54470d7d7f714788b36e44b1f6b1b82f5d56b49d
teoduongthanh <112098570+teoduongthanh@users.noreply.github.com>
2024-12-06 11:54:21 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 // Tạo kết nối đến cơ sở dữ liệu
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'dailydictation', // Tên database
-  process.env.DB_USER || 'root',          // Tên user
+  process.env.DB_NAME || '', // Tên database
+  process.env.DB_USER || '',          // Tên user
   process.env.DB_PASSWORD || '',          // Mật khẩu
   {
-    host: process.env.DB_HOST || 'localhost', // Địa chỉ host
+    host: process.env.DB_HOST || '', // Địa chỉ host
     dialect: 'mysql',                         // Loại cơ sở dữ liệu (MySQL)
     logging: false,                           // Tắt log SQL
   }
@@ -16,5 +16,6 @@ const sequelize = new Sequelize(
 sequelize.authenticate()
   .then(() => console.log('Connected to MySQL successfully'))
   .catch((err) => console.error('Unable to connect to MySQL:', err));
+
 
 module.exports = sequelize;
